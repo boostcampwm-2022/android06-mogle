fcfea158d7a79b9caa966be82c11787c97130b99
jhg3410 <80373033+jhg3410@users.noreply.github.com>
2022-11-16 15:42:39 +0900

    feat: 사진 여러장(최대 5장)을 버튼으로 추가하고, 삭제할 때는 추가한 것을 x 버튼으로 삭제한다.

--- presentation/src/main/java/com/wakeup/presentation/adapter/BindingAdapters.kt
+++ presentation/src/main/java/com/wakeup/presentation/adapter/BindingAdapters.kt
@@ -1,4 +1,22 @@
 package com.wakeup.presentation.adapter
 
-class BindingAdapters {
+import androidx.databinding.BindingAdapter
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+
+object BindingAdapters {
+
+    @JvmStatic
+    @BindingAdapter("submitList")
+    fun bindSubmitList(view: RecyclerView, itemList: List<Any>?) {
+        view.adapter?.let {
+            (view.adapter as ListAdapter<Any, *>).submitList(itemList)
+        }
+    }
+
+    @JvmStatic
+    @BindingAdapter("imageBitmap")
+    fun bindImageBitmap(view: android.widget.ImageView, bitmap: android.graphics.Bitmap?) {
+        view.setImageBitmap(bitmap)
+    }
 }
---
+++ presentation/src/main/java/com/wakeup/presentation/ui/addmoment/AddMomentViewModel.kt
@@ -0,0 +1,33 @@
+package com.wakeup.presentation.ui.addmoment
+
+import android.content.Intent
+import android.graphics.Bitmap
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+
+class AddMomentViewModel : ViewModel() {
+
+    fun getPictureIntent(): Intent {
+        return Intent(Intent.ACTION_PICK).apply {
+            type = "image/*"
+        }
+    }
+
+    private val _pictures = MutableStateFlow<List<Bitmap>>(emptyList())
+    val pictures: StateFlow<List<Bitmap>> = _pictures
+
+    private val _isPictureMax = MutableStateFlow(false)
+    val isPictureMax: StateFlow<Boolean> = _isPictureMax
+
+
+    fun addPicture(picture: Bitmap) {
+        _pictures.value = pictures.value + picture
+        _isPictureMax.value = pictures.value.size >= 5
+    }
+
+    fun removePicture(picture: Bitmap) {
+        _pictures.value = pictures.value - picture
+        _isPictureMax.value = pictures.value.size >= 5
+    }
+}
