ea83b68f781d52149a171ceb20cd243397a1ce7e
jaemin-Yoo <iamjm29@naver.com>
2022-11-17 13:41:33 +0900

    ci test

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,6 +11,5 @@ buildscript {
         classpath(com.wakeup.buildsrc.Depends.ClassPaths.hilt_android_gradle_plugin)
         classpath(com.wakeup.buildsrc.Depends.ClassPaths.kotlinx_serialization)
         classpath(com.wakeup.buildsrc.Depends.ClassPaths.google_services)
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.20")
     }
 }
