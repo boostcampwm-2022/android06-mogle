0bbde17801c97fd1b7374424b07eceb355fd21a1
BBongKim <kbg0099@naver.com>
2022-11-23 13:09:28 +0900

    design: 모먼트 상세 UI 구성

---
+++ presentation/src/main/java/com/wakeup/presentation/ui/detail/MomentDetailFragment.kt
@@ -0,0 +1,18 @@
+package com.wakeup.presentation.ui.detail
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.wakeup.presentation.R
+
+class MomentDetailFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View {
+        return inflater.inflate(R.layout.fragment_moment_detail, container, false)
+    }
+}
