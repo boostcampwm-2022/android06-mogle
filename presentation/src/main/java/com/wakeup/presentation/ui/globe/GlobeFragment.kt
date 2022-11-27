b2b01ec0d82134fff1e0e608577a81a588478fac
Choe-Ji-Hwan <ghldtjd901@naver.com>
2022-11-27 13:12:46 +0900

    feat: 상단 collapsingToolbar UI 구현

--- presentation/src/main/java/com/wakeup/presentation/ui/globe/GlobeFragment.kt
+++ presentation/src/main/java/com/wakeup/presentation/ui/globe/GlobeFragment.kt
@@ -5,10 +5,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import com.wakeup.presentation.databinding.FragmentGlobeBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class GlobeFragment : Fragment() {
 
+    private val viewModel: GlobeViewModel by viewModels()
     private lateinit var binding: FragmentGlobeBinding
 
     override fun onCreateView(
@@ -21,5 +26,11 @@ class GlobeFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        initToolbar()
+    }
+
+    private fun initToolbar() {
+        binding.tvIcon.setOnClickListener { findNavController().navigateUp() }
     }
 }
---
+++ presentation/src/main/java/com/wakeup/presentation/ui/globe/GlobeViewModel.kt
@@ -0,0 +1,12 @@
+package com.wakeup.presentation.ui.globe
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class GlobeViewModel @Inject constructor(
+
+): ViewModel() {
+    // todo
+}
