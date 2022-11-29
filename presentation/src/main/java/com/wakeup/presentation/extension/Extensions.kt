f04f981ca959e3108b419252ccbf6034bf158666
jaemin-Yoo <iamjm29@gmail.com>
2022-11-29 11:32:31 +0900

    feat: 피드백 반영
    - hideKeyboard 확장함수 관리
    - mogle icon svg 파일 추가
    - Weather theme enum class 관리

--- presentation/src/main/java/com/wakeup/presentation/extension/Extensions.kt
+++ presentation/src/main/java/com/wakeup/presentation/extension/Extensions.kt
@@ -1,5 +1,10 @@
 package com.wakeup.presentation.extension
 
+import android.content.Context
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.MutableLiveData
 import androidx.navigation.NavController
 
@@ -9,3 +14,12 @@ fun <T> NavController.setNavigationResultToBackStack(key: String, value: T) {
 
 fun <T> NavController.getNavigationResultFromTop(key: String): MutableLiveData<T>? =
     this.currentBackStackEntry?.savedStateHandle?.getLiveData(key)
+
+fun Fragment.hideKeyboard() {
+    view?.let { activity?.hideKeyboard(it) }
+}
+
+fun Context.hideKeyboard(view: View) {
+    val imm = getSystemService(AppCompatActivity.INPUT_METHOD_SERVICE) as InputMethodManager
+    imm.hideSoftInputFromWindow(view.windowToken, 0)
+}
---
+++ presentation/src/main/java/com/wakeup/presentation/model/WeatherTheme.kt
@@ -0,0 +1,5 @@
+package com.wakeup.presentation.model
+
+enum class WeatherTheme(val str: String) {
+    AUTO("자동"), BRIGHT("낮"), DARK("밤")
+}
