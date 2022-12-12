caa20fa56de4b821e109ec8f18eb68af698a1ebf
bbongkim <kbg0099@naver.com>
2022-12-13 04:49:16 +0900

    fix: 모먼트 삭제 버그 수정

--- domain/src/main/java/com/wakeup/domain/usecase/globe/DeleteMomentInGlobeUseCase.kt
+++ domain/src/main/java/com/wakeup/domain/usecase/globe/DeleteMomentInGlobeUseCase.kt
@@ -15,7 +15,7 @@ class DeleteMomentInGlobeUseCase @Inject constructor(
 
         val firstMoment = globeRepository.getFirstMomentByGlobe(globe.id)
 
-        if (firstMoment == null) {
+        if (firstMoment == null || firstMoment.pictures.isEmpty()) {
             globeRepository.updateGlobe(globe.copy(thumbnail = null))
         } else {
             globeRepository.updateGlobe(globe.copy(thumbnail = firstMoment.pictures.first()))
